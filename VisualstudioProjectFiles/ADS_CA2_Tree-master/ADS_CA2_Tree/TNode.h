8cb434053c10cc9edc36c24255f7afe5b9bd1c37
ArnyVR <99402337+ArnyVR@users.noreply.github.com>
2022-11-24 14:59:15 +0000

    ✨ Creating Binary Tree
    Trying to create binary tree

--- VisualstudioProjectFiles/ADS_CA2_Tree-master/ADS_CA2_Tree/TNode.h
+++ VisualstudioProjectFiles/ADS_CA2_Tree-master/ADS_CA2_Tree/TNode.h
@@ -3,6 +3,12 @@
 template <typename K, typename E>
 class TNode {
 private:
+
+	
+
+
+
+
 	/// <summary>
 	/// A subset of the main data used to generate a unique number for the node
 	/// </summary>
@@ -15,13 +21,32 @@ private:
 	/// <summary>
 	/// Pointers to the two kids. Go left if new data key < key for this node, else go right
 	/// </summary>
-	TNode* pLeft, * pRight;
+	TNode* pLeft, * pRight, *parent;
+
 
 public:
+
+	TNode();
+
 	bool insert(K key, E data) {
 		return false;
 	}
 
 	void setData(E data) { this->data = data; }
 	E getData() const { return this->data; }
+
+
+	template <typename K, typename E>
+	TNode<K,E>::TNode()
+	{
+		parent = nullptr;
+		left = nullptr;
+		right = nullptr;
+	}
+
+
+
+
+
+
 };
